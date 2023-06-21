122c7c65f1f397dac838d6265e9e145ce813de4c
Filipe Pires <filipepmachado1997@gmail.com>
2023-06-20 21:41:38 -0300

    alteraçoes no email

--- resources/views/mail/informativeMovieUpdatedMail.blade.php
+++ resources/views/mail/informativeMovieUpdatedMail.blade.php
@@ -2,13 +2,15 @@
 
 <h1>Olá, {{$user->name}}!</h1>
 
-<p>Informamos que você acabou de fazer uma alteração nos seus filmes</p>
+<p>Informamos que você acabou de fazer uma alteração nos seus filmes!</p>
 
-<img src="{{$movie->img_src}}" style="width: 90px;">
+<div style="display: flex; justify-content: center;">
+    <img src="{{$movie->img_src}}" style="width: 90px; border-radius: 5px;">
+</div>
 <p><b>Filme: </b>{{$movie->name}}</p>
 <p><b>Nota: </b>{{$data_movies->note}}</p>
 <p><b>Assitido em: </b>{{date("d/m/Y", strtotime($data_movies->assisted_in))}}</p>
-<p><b>Suas Observações</b><br>
+<p><b>Suas Observações:</b><br>
     {{$data_movies->observation}}
 </p>
 
