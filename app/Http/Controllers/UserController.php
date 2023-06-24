741cd145d113332af1235e4296054dbc5ed82cf9
Filipe Pires <filipepmachado1997@gmail.com>
2023-06-23 23:59:31 -0300

    criação profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('profile.profile');
+        $user = DB::table('users')->where('id', auth()->user()->id)->first();
+        return view('profile.profile', compact('user'));
     }
 }
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -6,30 +6,22 @@
 
     <form action="" method="post">
         @csrf
-        <div class="flex container mx-auto p-2 w-full bg-gray-700 rounded-lg mt-3">
-            <div class="flex">
-                <div class="grid justify-items-center px-3 py-3">
-                    <img class="rounded-full shadow-2xl md: w-64" src="{{ url('storage/minha-foto.jpg') }}" alt="">
-                </div>
+        <div class="flex container mx-auto p-2 bg-gray-700 rounded-lg mt-3" style="width:335px;">
+            <div class="grid justify-items-start px-3 py-3">
+                <label for="name" class="block font-semibold text-yellow-500">Nome</label>
+                <input class="border text-base px-2 py-1 rounded-md focus:outline-none focus:ring-0 focus:border-yellow-400 mb-6 w-72" type="text" name="name" id="name" value="{{$user->name}}" placeholder="Digite seu nome">
+                
+                <label for="last-name" class="block font-semibold text-yellow-500">Sobrenome</label>
+                <input class="border text-base px-2 py-1 rounded-md focus:outline-none focus:ring-0 focus:border-yellow-400 mb-6 w-72" type="text" name="last-name" id="last-name" value="{{$user->last_name}}" placeholder="Digite seu sobrenome">
 
-                <div>
-                    <label for="name">Nome</label>
-                    <input class="border text-base px-2 py-1 rounded-md focus:outline-none focus:ring-0 focus:border-yellow-400 mb-6" type="text" name="name" id="name" value="" placeholder="Digite seu nome">
-                    
-                    <label for="last-name">Sobrenome</label>
-                    <input class="border text-base px-2 py-1 rounded-md focus:outline-none focus:ring-0 focus:border-yellow-400 mb-6" type="text" name="last-name" id="last-name" value="" placeholder="Digite seu sobrenome">
-                
-                    <label for="email">E-mail</label>
-                    <input class="border text-base px-2 py-1 rounded-md focus:outline-none focus:ring-0 focus:border-yellow-400 mb-6" type="text" name="last-name" id="last-name" value="filipepmachado@gmail.com" disabled>
-                
-                
-                    <label for="recive-email">Deseja receber emails de atualização</label>
-                    <input type="checkbox" name="recive-email">
-                
-                
-                </div>
-                
-
+                <label for="email" class="block font-semibold text-yellow-500">E-mail</label>
+                <input class="border text-base px-2 py-1 rounded-md focus:outline-none focus:ring-0 focus:border-yellow-400 mb-6 w-72" type="text" name="last-name" id="last-name" value="{{$user->email}}" disabled>
+                            
+                <label for="recive_email" class="block font-semibold text-yellow-500">Deseja receber emails de atualização ?</label>
+                <select name="recive_email" id="recive_email" class="border text-base px-2 py-1 rounded-md focus:outline-none focus:ring-0 focus:border-yellow-400 mb-6 w-72">
+                    <option value="t" <?=($user->recive_email == 't') ? 'selected' : ''?>>Sim</option>
+                    <option value="f" <?=($user->recive_email == 'f') ? 'selected' : ''?>>Não</option>
+                </select>               
             </div>
         </div>
 
